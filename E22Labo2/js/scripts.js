e4b1cc8b86d1cf5b5c57701f233835b5f751cd7a
monirrafi <programermonirrafi@gmail.com>
2022-07-07 23:34:45 -0400

    save

--- E22Labo2/js/scripts.js
+++ E22Labo2/js/scripts.js
@@ -1,6 +1,27 @@
-// En ES5
-function choixMusique(){
-  let sel = document.getElementById('idmusique');
-  let optionChoisie = sel.options[sel.selectedIndex].text;
-  alert("Vous avez choisit la catégorie : "+optionChoisie);
+let chemin = "../interets/";
+let listeInteret = [
+  {"id":1,"link":"musique.html"},
+  {"id":2,"link":"loisir.html"},
+  {"id":3,"link":"musique.html"},
+  {"id":4,"link":"loisir.html"}
+];
+
+let remplirInteretDansSelect = () =>{
+  document.getElementById('idframe').src = chemin+"musique.html";
+  let selInteret = document.getElementById('selInteret');
+  for(let unInteret of listeInteret){
+    selInteret.options[selInteret.options.length] = new Option(unInteret.id, unInteret.link);
+  }
+}
+let traiterInterert = () => {
+  
+  let selInteret = document.getElementById('selInteret');
+  let idInteret = selInteret.options[selInteret.selectedIndex].value;
+  let objInteret = listeInteret.find(unInteret => unInteret.id == idInteret);
+  let srcInteret = objInteret.id;
+  
+ // document.getElementById('frameErr').innerHTML = srcInteret;
+
+  document.getElementById('idframe').src = chemin + srcInteret;
+
 }
