bb71546c046503b4d6e676e5b103d867e531c389
monirrafi <100225631+monirrafi@users.noreply.github.com>
2022-07-08 10:51:10 -0400

    save

--- E22Labo2/js/scripts.js
+++ E22Labo2/js/scripts.js
@@ -1,25 +1,43 @@
 let chemin = "../interets/";
+/*
 let listeInteret = [
   {"id":1,"link":"musique.html"},
   {"id":2,"link":"loisir.html"},
   {"id":3,"link":"musique.html"},
   {"id":4,"link":"loisir.html"}
 ];
-
+*/
+let listeInteret = [
+  "musique","soccer","dessin","autre"
+]
+let listeSite = [
+  "https://en.wikipedia.org/wiki/Music","https://www.fifa.com","https://www.fifa.com",
+  "https://www.pbs.org/wnet/nature"
+]
 let remplirInteretDansSelect = () =>{
-  document.getElementById('idframe').src = chemin+"musique.html";
+  document.getElementById('idframe').src = listeSite[0];
   let selInteret = document.getElementById('selInteret');
   for(let unInteret of listeInteret){
-    selInteret.options[selInteret.options.length] = new Option(unInteret.id, unInteret.link);
+    selInteret.options[selInteret.options.length] = new Option(unInteret);
   }
 }
 let traiterInterert = () => {
-  
+
   let selInteret = document.getElementById('selInteret');
-  let idInteret = selInteret.options[selInteret.selectedIndex].value;
+  let idInteret = selInteret.options[selInteret.selectedIndex].text;
+  /*
   let objInteret = listeInteret.find(unInteret => unInteret.id == idInteret);
-  let srcInteret = objInteret.id;
   
-  document.getElementById('idframe').src = chemin + srcInteret;
+  let srcInteret;
+ */
+  let index = listeInteret.findIndex( interet => interet == idInteret);
+ // for(let unInteret of listeInteret){
+  //  if(unInteret == idInteret){
+      document.getElementById('idframe').src = listeSite[index];
+
+  //  }
+//    selInteret.options[selInteret.options.length] = new Option(unInteret);
+ // } 
+  
 
 }
